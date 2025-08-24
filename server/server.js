f4781eb5b9f9e04b7ed67f23fcb1b2edf2bfb37d
sujal-17-nagpal <sujaln0609y@gmail.com>
2025-08-24 12:09:02 +0530

    vercel updates

--- server/server.js
+++ server/server.js
@@ -1,54 +1,58 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import http from 'http';
-import { connectDb } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-import { Server } from "socket.io"
+import cors from "cors";
+import http from "http";
+import { connectDb } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
-const app = express()
+const app = express();
 
-const server = http.createServer(app)
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+const server = http.createServer(app);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 //initialize socket.io server
-export const io = new Server(server,{cors:{origin:"*"}})
+export const io = new Server(server, { cors: { origin: "*" } });
 
 //store online users
 export const userSocketMap = {}; // {userId : socketId}
 
 // Socket.io connection handler
-io.on("connection",(socket)=>{
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected",userId)
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id
+  if (userId) userSocketMap[userId] = socket.id;
 
-    // Emit online users to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected",userId)
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
- 
-app.use('/api/status',(req,res)=>{
-    res.send("Server is live")
-})
+app.use("/api/status", (req, res) => {
+  res.send("Server is live");
+});
 
-app.use('/api/auth',userRouter)
-app.use('/api/messages',messageRouter)
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
-await connectDb()
+await connectDb();
 
-const PORT = process.env.PORT || 5000
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-server.listen(PORT,()=>{
-    console.log(`server in running on port: ${PORT}`)
-})
+  server.listen(PORT, () => {
+    console.log(`server in running on port: ${PORT}`);
+  });
+}
+
+//export server for vercel
+export default server
