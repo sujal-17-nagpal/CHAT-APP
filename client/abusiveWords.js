550635b4953dc67d12bb8c74f6fb22938146a44a
sujal-17-nagpal <sujaln0609y@example.com>
2026-01-22 17:44:35 +0530

    trie for masking abusive words

---
+++ client/abusiveWords.js
@@ -0,0 +1,11 @@
+// List of abusive/inappropriate words to be masked in messages
+export const abusiveWords = [
+  "badword1",
+  "badword2",
+  "badword3",
+  "offensive",
+  "inappropriate",
+  "rude",
+  "vulgar",
+  
+];
